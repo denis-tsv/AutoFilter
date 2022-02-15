108f9fb0354447dd51e7f10833a0dd99eb6885b4
Denis <yadeniskotcvet@gmail.com>
2022-02-16 06:45:09 +0700

    Not necessary to mark properties which doesn't exists in target type (like SortBy) with NotAutoFiltered attribute (#3)

--- AutoFilter/FilterProperty.cs
+++ AutoFilter/FilterProperty.cs
@@ -5,6 +5,7 @@ namespace AutoFilter
 {
     public class FilterProperty<TFilter>
     {
+        public bool HasAttribute { get; set; }
         public Func<TFilter, object> PropertyValueGetter { get; set; }
         public PropertyInfo PropertyInfo { get; set; }
         public FilterPropertyAttribute FilterPropertyAttribute { get; set; }        
