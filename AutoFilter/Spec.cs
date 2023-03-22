43995c3a7117193b8ee3f582e93243d3e49c27fa
Denis <yadeniskotcvet@gmail.com>
2023-03-22 16:28:47 +0700

    refactor

--- AutoFilter/Spec.cs
+++ AutoFilter/Spec.cs
@@ -11,27 +11,24 @@ public class Spec<T>
     public static bool operator true(Spec<T> _) => false;
 
     public static Spec<T> operator &(Spec<T> spec1, Spec<T> spec2)
-        => new(spec1._expression.And(spec2._expression));
+        => new(spec1.Expression.And(spec2.Expression));
 
     public static Spec<T> operator |(Spec<T> spec1, Spec<T> spec2)
-        => new(spec1._expression.Or(spec2._expression));
+        => new(spec1.Expression.Or(spec2.Expression));
 
     public static Spec<T> operator !(Spec<T> spec)
-        => new(spec._expression.Not());
+        => new(spec.Expression.Not());
 
     public static implicit operator Expression<Func<T, bool>>(Spec<T> spec)
-        => spec._expression;
+        => spec.Expression;
 
     public static implicit operator Spec<T>(Expression<Func<T, bool>> expression)
         => new(expression);
 
-    private readonly Expression<Func<T, bool>> _expression;
+    public Expression<Func<T, bool>> Expression { get; }
 
     public Spec(Expression<Func<T, bool>> expression)
     {
-        _expression = expression ?? throw new ArgumentNullException(nameof(expression));
+        Expression = expression ?? throw new ArgumentNullException(nameof(expression));
     }
-
-    // don't used because compilation of expression takes a long time
-    public bool IsSatisfiedBy(T obj) => _expression.Compile()(obj);
 }
